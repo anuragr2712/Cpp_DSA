06f6ce0728cd53a53025f0374b22b2bd9b48ea02
anuragr2712 <anurag.git2712@gmail.com>
2025-07-15 17:35:29 -0700

    Operators

---
+++ Day3/constants.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << (10/3) << endl;
+    cout << (10/3.0) << endl;
+    cout << ('A' + 1) << endl;
+    return 0;
+}
---
+++ Day3/explicit_typecast.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     float PI = 3.14;
+//     cout << (int)(PI)<<endl;
+//     cout <<( (float)10/3)<<endl;
+//     cout <<(char)('A'+1)<<endl;
+//     return 0;
+// }
+
+int main(){
+    cout << (bool)3+2<<endl;
+    cout << (23.5 + 2 + 'A') << endl;
+    return 0;
+}
